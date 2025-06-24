f56901a0c58ac8326e0716f2165de95f776084a7
Guilherme Lima <guilhermefk123@gmail.com>
2025-06-23 22:17:08 -0300

    Ajustes iniciais do projeto

---
+++ src/main.js
@@ -0,0 +1,23 @@
+class ListaDeTarefas {
+    constructor() {
+        this.lista = [];
+        this.idAtual = 1;
+    }
+
+    marcarComoFeita() {
+
+    }
+
+    apagar() {
+
+    }
+
+    mostrarNaTela() {
+
+
+    }
+
+    atualizarContagem() {
+
+    }
+}
